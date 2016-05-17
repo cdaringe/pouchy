4b968ae93684c08384d8674970038f2dd2a4d45b
cdaringe <christopher.dieringer@gmail.com>
2016-05-17 12:11:30 -0600

    good grief bluebird, relax.

--- src/bluebirdify-pouchdb.js
+++ src/bluebirdify-pouchdb.js
@@ -4,11 +4,7 @@ var bb = require('bluebird')
 var __Promise = global.Promise
 global.Promise = bb
 var PouchDB = require('pouchdb')
-bb.config({
-  warnings: {
-    wForgottenReturn: false
-  }
-})
+bb.config({ warnings: false })
 PouchDB.utils.Promise = bb
 global.Promise = __Promise
 PouchDB.plugin(require('pouchdb-find'))
